66541b9569c03acdd845a4986a7b1346f7817271
wjryan <43651659+wjryan@users.noreply.github.com>
2018-10-09 11:25:15 -0700

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = 1.0 #define a float
+y = 2.0	#define another float
+
+#trigonometry
+print(np.sin(x))
+print(np.cos(x))
+print(np.tan(x))
+print(np.arcsin(x))
+print(np.arccos(x))
+print(np.arctan(x))
+print(np.arctan2(x))	#arctan(x/y)
+print(np.rad2deg(x))	#convert rad to deg
+
+#hyperbolic functions
+print(np.sinh(x))
+print(np.cosh(x))
+print(np.tanh(x))
+print(np.arcsinh(x))
+print(np.arccosh(x))
+print(np.arctanh(x))
